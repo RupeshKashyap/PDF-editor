3bc78af5dd075f84d7d924973d57cde6a765e640
Rupesh Rupesh <rupeshkashyap1996@gmail.com>
2020-05-10 15:20:18 +0530

    first commit

---
+++ src/main/java/com/rupesh/PdfEditorApplication.java
@@ -0,0 +1,13 @@
+package com.rupesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfEditorApplication.class, args);
+	}
+
+}
